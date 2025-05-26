98b8b2acbf66c5e83f97f313bb4255e1d95d7ece
waydroirdtest <waydroirdtest@gmail.com>
2025-05-26 12:53:24 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -9,8 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://huggingface.co/spaces/waytest/nuovo-spazio-mfp"     # non mettere lo / finale al link
+MFPPSW = "relitto"
 
 # Costanti
 NUM_CHANNELS = 10000
