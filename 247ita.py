b994c53ab2ca3dc8c10df18a042f7f5ece2d6dc5
waydroirdtest <waydroirdtest@gmail.com>
2025-06-21 16:31:51 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -5,8 +5,9 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK= "https://waytest-hf-mfptest.hf.space"
+MFPPSW= "prova"
+
 
 # Costanti
 M3U8_OUTPUT_FILE = "247ita.m3u8"
