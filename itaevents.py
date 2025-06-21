dcd8e2750c0e84b679af17bf1fc0cd6b58fb22b6
waydroirdtest <waydroirdtest@gmail.com>
2025-06-21 16:32:53 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK= "https://waytest-hf-mfptest.hf.space"
+MFPPSW= "prova"
 
 
 # Constants
